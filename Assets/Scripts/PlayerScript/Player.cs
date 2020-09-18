56da5f5357b2f541f6be14b841dd3cb46a6e2b22
Muhammad Nadzmi <nadzmiidzham@gmail.com>
2020-09-18 22:13:56 +0800

    Added movement & jump function

--- Assets/Scripts/PlayerScript/Player.cs
+++ Assets/Scripts/PlayerScript/Player.cs
@@ -7,18 +7,39 @@ public class Player : MonoBehaviour
     public float hp, maxHp;
     public float ep, maxEp;
     public float atk, def, spd;
+    public float jumpPower;
 
     bool isFacingRight = true;
+    [SerializeField] BoxCollider2D groundCheck;
+    [SerializeField] LayerMask groundLayer;
     Animator animator;
     Rigidbody2D rigidbody2D;
+    BoxCollider2D boxCollider2D;
 
     private void Start()
     {
         animator = GetComponent<Animator>();
         rigidbody2D = GetComponent<Rigidbody2D>();
+        boxCollider2D = GetComponent<BoxCollider2D>();
     }
 
     private void FixedUpdate()
+    {
+        if (IsGrounded() && Input.GetKey(KeyCode.Space))
+        {
+            Debug.Log(IsGrounded());
+            Jump();
+        }
+
+        Movement();
+    }
+
+    void Jump()
+    {
+        rigidbody2D.velocity = Vector2.up * jumpPower * Time.deltaTime;
+    }
+
+    void Movement()
     {
         float direction = Input.GetAxisRaw("Horizontal");
         float movement = direction * spd * Time.deltaTime;
@@ -27,7 +48,7 @@ public class Player : MonoBehaviour
         {
             Flip();
         }
-        else if(isFacingRight && (direction < 0))
+        else if (isFacingRight && (direction < 0))
         {
             Flip();
         }
@@ -42,5 +63,10 @@ public class Player : MonoBehaviour
         Vector3 thisScale = transform.localScale;
         thisScale.x *= -1;
         transform.localScale = thisScale;
+    }
+
+    bool IsGrounded()
+    {
+        return groundCheck.IsTouchingLayers(groundLayer);
     }
 }
