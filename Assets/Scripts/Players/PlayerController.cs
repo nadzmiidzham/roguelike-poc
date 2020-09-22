cb51818d191e6fd5ff44824e1fafa4d35d6fdf5d
Muhammad Nadzmi <nadzmiidzham@gmail.com>
2020-09-23 00:21:11 +0800

    - changed player movement script

--- Assets/Scripts/Players/PlayerController.cs
+++ Assets/Scripts/Players/PlayerController.cs
@@ -2,40 +2,128 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : PhysicObject
+public class PlayerController : MonoBehaviour
 {
-    [Header("Player Behaviour")]
     public PlayerState playerState;
+    public LayerMask groundlayer;
+    public Vector2 bottomOffset, rightOffset;
+    public float collisionRadius;
+    public float fallMultiplier = 2.5f;
+    public float lowJumpMultiplier = 2f;
+    public float slideSpeed;
 
-    private SpriteRenderer spriteRenderer;
+    new Rigidbody2D rigidbody;
+    SpriteRenderer spriteRenderer;
+
+    bool onGround = false;
+    bool onWall = false;
+    bool isFacingRight = true;
+    bool isWallJump = false;
 
     void Awake()
     {
+        rigidbody = GetComponent<Rigidbody2D>();
         spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
-    protected override void ComputeVelocity()
+    void Update()
     {
-        Vector2 move = Vector2.zero;
+        Vector2 direction = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
 
-        move.x = Input.GetAxis("Horizontal");
+        onGround = Physics2D.OverlapCircle((Vector2)transform.position + bottomOffset, collisionRadius, groundlayer);
+        onWall = Physics2D.OverlapCircle((Vector2)transform.position + rightOffset, collisionRadius, groundlayer);
 
-        if (Input.GetButtonDown("Jump") && grounded)
+        // add gravity based on how long the jump
+        if (rigidbody.velocity.y < 0)
         {
-            velocity.y = playerState.jumpForce;
+            rigidbody.velocity += Vector2.up * Physics2D.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
         }
-        else if (Input.GetButtonUp("Jump"))
+        else if ((rigidbody.velocity.y > 0) && !Input.GetButton("Jump"))
         {
-            velocity.y = (velocity.y > 0) ? 0.5f : velocity.y;
-        }
-
-        Debug.Log(move.x);
-        bool flipSprite = spriteRenderer.flipX ? (move.x > 0) : (move.x < 0);
-        if (flipSprite)
-        {
-            spriteRenderer.flipX = !spriteRenderer.flipX;
+            rigidbody.velocity += Vector2.up * Physics2D.gravity.y * (lowJumpMultiplier - 1) * Time.deltaTime;
         }
 
-        targetVelocity = move * playerState.spd;
+        // flip sprite
+        if ((direction.x > 0) && !isFacingRight)
+        {
+            Flip();
+        }
+        if ((direction.x < 0) && isFacingRight)
+        {
+            Flip();
+        }
+
+        // check is wall jump
+        if (onGround && isWallJump)
+        {
+            isWallJump = false;
+        }
+
+        // walk
+        if (!isWallJump)
+        {
+            Walk(direction);
+        }
+
+        // jump
+        if (Input.GetButtonDown("Jump") && onGround)
+        {
+            Jump(Vector2.up);
+        }
+
+        // wall jump
+        if (Input.GetButtonDown("Jump") && onWall && !onGround)
+        {
+            WallJump(Vector2.right);
+            isWallJump = true;
+        }
+        if (onWall && !onGround)
+        {
+            WallSlide();
+        }
+    }
+
+    void Walk(Vector2 direction)
+    {
+        rigidbody.velocity = (new Vector2(direction.x * playerState.spd, rigidbody.velocity.y));
+    }
+
+    void Jump(Vector2 direction)
+    {
+        rigidbody.velocity = new Vector2(rigidbody.velocity.x, 0);
+        rigidbody.velocity += direction * playerState.jumpForce;
+    }
+
+    void WallJump(Vector2 direction)
+    {
+        rigidbody.velocity = new Vector2(rigidbody.velocity.x, 0);
+        rigidbody.velocity = new Vector2(playerState.jumpForce, playerState.spd);
+        Debug.Log(rigidbody.velocity);
+    }
+
+    void WallSlide()
+    {
+        rigidbody.velocity = new Vector2(rigidbody.velocity.x, -slideSpeed);
+    }
+
+    void Flip()
+    {
+        isFacingRight = !isFacingRight;
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+
+        // change bottom & right offset
+        bottomOffset.x *= -1;
+        rightOffset.x *= -1;
+    }
+
+    void OnDrawGizmos()
+    {
+        var position = new Vector2[] { bottomOffset, rightOffset };
+
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere((Vector2)transform.position + bottomOffset, collisionRadius);
+        Gizmos.DrawWireSphere((Vector2)transform.position + rightOffset, collisionRadius);
     }
 }
